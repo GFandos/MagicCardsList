7bbd0440b21a443c989b061a81eb68319785501a
gfandos <gerard.fandos@gmail.com>
2016-10-18 19:59:43 +0200

    Starting the project (ignore the other 2 first commits): testing the listView

--- app/src/main/java/com/example/a47989768s/magiccardslist/MainActivityFragment.java
+++ app/src/main/java/com/example/a47989768s/magiccardslist/MainActivityFragment.java
@@ -5,11 +5,19 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+
+    private ArrayList<String> items;
+    private ArrayAdapter<String> adapter;
 
     public MainActivityFragment() {
     }
@@ -17,6 +25,25 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        ListView cardsList = (ListView) view.findViewById(R.id.cardsList);
+
+        String [] data = {"Carta 1", "Carta 2", "Carta 3", "Carta 4", "Carta 5", "Carta 6", "Carta 7"};
+        items = new ArrayList<>(Arrays.asList(data));
+
+        adapter = new ArrayAdapter<>(
+                getContext(),
+                R.layout.cards_rows,
+                R.id.cardName,
+                items
+        );
+
+        cardsList.setAdapter(adapter);
+
+        return view;
+
+
     }
 }
