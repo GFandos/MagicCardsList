b9d48e6d140ef899b3930b3b6bc08aa3c4c0cacd
gfandos <gerard.fandos@gmail.com>
2016-10-18 18:14:07 +0200

    We create HttpUtil for internet needs and Card class to store our cards

---
+++ app/src/main/java/com/example/a47989768s/magiccardslist/Card.java
@@ -0,0 +1,45 @@
+package com.example.a47989768s.magiccardslist;
+
+/**
+ * Created by 47989768s on 18/10/16.
+ */
+
+public class Card {
+
+    private String name;
+    private String type;
+    private String rarity;
+    private String imgURL;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getRarity() {
+        return rarity;
+    }
+
+    public void setRarity(String rarity) {
+        this.rarity = rarity;
+    }
+
+    public String getImgURL() {
+        return imgURL;
+    }
+
+    public void setImgURL(String imgURL) {
+        this.imgURL = imgURL;
+    }
+}
