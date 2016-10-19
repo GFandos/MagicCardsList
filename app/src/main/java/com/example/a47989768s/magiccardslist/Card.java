e0c1b2cc465ce68980ced42cae7b222a533c2a17
gfandos <gerard.fandos@gmail.com>
2016-10-19 16:32:02 +0200

    POJO created to store cards information

---
+++ app/src/main/java/com/example/a47989768s/magiccardslist/Card.java
@@ -0,0 +1,49 @@
+package com.example.a47989768s.magiccardslist;
+
+/**
+ * Created by 47989768s on 19/10/16.
+ */
+
+public class Card {
+
+    private String name;
+    private String rarity;
+    private String type;
+
+    public Card() {
+
+    }
+
+    @Override
+    public String toString() {
+        return "Card{" +
+                "name='" + name + '\'' +
+                ", rarity='" + rarity + '\'' +
+                ", type='" + type + '\'' +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRarity() {
+        return rarity;
+    }
+
+    public void setRarity(String rarity) {
+        this.rarity = rarity;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
