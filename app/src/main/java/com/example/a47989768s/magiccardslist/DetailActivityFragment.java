6eb0f660688d9214fec026e9c7496611d05ea86a
gfandos <gerard.fandos@gmail.com>
2016-11-08 17:52:41 +0100

    Data of the selected card is taken and shown on the log to check if it works

--- app/src/main/java/com/example/a47989768s/magiccardslist/DetailActivityFragment.java
+++ app/src/main/java/com/example/a47989768s/magiccardslist/DetailActivityFragment.java
@@ -1,7 +1,9 @@
 package com.example.a47989768s.magiccardslist;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,6 +19,23 @@ public class DetailActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_detail, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_detail, container, false);
+
+        Intent i = getActivity().getIntent();
+
+        if(i != null) {
+            Card card = (Card) i.getSerializableExtra("card");
+
+            if(card != null) {
+                updateUI(card);
+            }
+        }
+
+        return view;
+    }
+
+    private void updateUI(Card card) {
+        Log.d("MOVIE", card.toString());
     }
 }
